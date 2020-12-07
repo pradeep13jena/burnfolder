14bc6c1526f4d360ee1d0358422c185c99cd150c
Pradeep Jena <pj39467552@gmail.com>
2020-12-07 18:19:29 +0530

    ban rha hai

---
+++ frontpage.php
@@ -0,0 +1,61 @@
+<?php
+	include 'header.php';
+?>
+<!-- SEARCH SECTION -->
+<div class="container">
+	<div class="jumbotron" id="forsearch">
+			<h1 class="text-center SEARCH_H1"> Search Your Product</h1>
+		<form>
+			<div class="form-group">
+				<input type="Search" placeholder="Search..." name="Searchbar" class="form-control">
+			</div>
+		</form>
+	</div>
+</div>
+<!-- LOOK SECTION -->
+<div class="jumbotron">
+	<div class="container">
+		<h1 class="exploretext">Explore</h1>
+		<div class="row">
+			<div class="col-lg-3 col-md-6 col-sm-6 colfortiles">
+				<a href="#">
+					<div class="booktile d-flex justify-content-center">
+						<img src="image/books.png" width="100%" height="250px">
+						<span>BOOKS</span>
+					</div>
+				</a>
+			</div>
+			<div class="col-lg-3 col-md-6 col-sm-6 colfortiles">
+				<a href="#">
+					<div class="phonetile d-flex justify-content-center">
+						<img src="image/Phone.jpeg" width="100%" height="250px">
+						<span>MOBILE</span>
+					</div>
+				</a>
+			</div>
+			<div class="col-lg-3 col-md-6 col-sm-6 colfortiles">
+				<a href="#">
+					<div class="laptoptile d-flex justify-content-center">
+						<img src="image/laptop.jpeg" width="100%" height="250px">
+						<span>LAPTOP</span>
+					</div>
+				</a>
+			</div>
+			<div class="col-lg-3 col-md-6 col-sm-6 colfortiles">
+				<a href="#">
+					<div class="headphonetile d-flex justify-content-center">
+						<img src="image/earphone.jpeg" width="100%" height="250px">
+						<span>EAR-PHONES</span>
+					</div>
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+<!-- BOOTSTRAP & MYOWN JAVASCRIPT LIBRARY -->
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/style.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@500&display=swap" rel="stylesheet">
+</head>
+<body>
+<header>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<a href="frontpage.php" class="navbar-brand"><img class="navbar-brand-image" src="image/sypp.jpeg"></a>
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-item">
+				<a class="nav-link" href="#">ABOUT</a>
+			</li>
+		</ul>
+		<ul class="navbar-nav">
+			<li class="nav-item">
+				<a href="#" class="btn btn-login">LOGIN</a>
+			</li>
+		</ul>
+</nav>
+</header>
